c589586ca1c87eec5ae89fab2f4b4c581abd82fa
aliturkes <aliturkes57@gmail.com>
2025-01-20 00:27:39 +0100

    Token serializers added.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -80,7 +80,7 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertNotIn('token', res.data)
+        self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_bad_credentials(self):
@@ -107,3 +107,4 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+4
