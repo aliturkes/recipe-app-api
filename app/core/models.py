02545b01032f634be868503a8dcd0ea37a06f8f9
aliturkes <aliturkes57@gmail.com>
2025-01-23 22:05:42 +0100

    models for tag is added

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@
 Database models.
 """
 
+from pyexpat import model
 from django.conf import settings
 from statistics import mode
 from unicodedata import name
@@ -58,7 +59,19 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tagds = models.ManyToManyField('Tag')
 
     def __str__(self):
-        return self.title 
+        return self.title
+
+class Tag(models.Model):
+    """Tag for filtering recipes"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
 
